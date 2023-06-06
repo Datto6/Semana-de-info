4af21b7cface2747eed9b47811f757ac9a56d5c5
Datto6 <61701645+Datto6@users.noreply.github.com>
2023-06-06 18:56:13 -0300

    novo metodo usando pandas classes.py

--- classes.py
+++ classes.py
@@ -1,39 +1,21 @@
+import pandas as pd
+respostas= pd.read_csv('respostas.csv')
 class Pessoa:
-    def __init__(self,nome, preferencias):
-       self.nome=nome
-       self.preferencias=preferencias
+    def __init__(self,informacoes):
+        self.data=informacoes.loc['Carimbo de data/hora']
+        self.nome=informacoes.loc['Nome']
     def nomear(self):
         return self.nome
     def aulas(self):
         return self.preferencias
 
-def contains_number(string):
-    return any(char.isdigit() for char in string)
-
-def nome_ou_nao(string,materias):
-    if  not contains_number(string) and  string not in materias:
-        if string != 'Materia' and string != 'Carimbo' and string != 'de' and string != 'data/hora':
-            return True
-    else:
-        return False 
-
-def separar(lista,materias):
+def separar(lista):
     alunos=[]
-    preferencias=[]
-    for word in lista:
-        nomes=[]
-        if nome_ou_nao(word,materias):
-            nomes.append(word)
-        elif word in materias:
-            preferencias.append(word)
-        alunos.append(Pessoa(nomes,materias))
+    for indexo,fileira in lista.iterrows():
+        alunos.append(Pessoa(fileira))
     return alunos
 
-file=open('info.txt')
-lista_total=file.read()
-lista_analisada=lista_total.split()
-conjunto_alunos=separar(lista_analisada,['Engenharia','Quimica','Arte','Foto'])
-for i in conjunto_alunos:
-    print(i.nomear())
 
-print(lista_analisada)
+produto=separar(respostas)
+for aluno in produto:
+    print(aluno.nomear())
